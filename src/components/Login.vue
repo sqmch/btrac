de3cf83078997baab086d1ca9e47e7f95f882ffa
sqmch <sqmchh@gmail.com>
2021-04-04 11:17:34 +0100

    Changes to login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,87 +1,91 @@
 <template>
-  <form>
-    <v-text-field
-      v-model="email"
-      :error-messages="emailErrors"
-      label="E-mail"
-      required
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
-    ></v-text-field>
-    <v-text-field
-      v-model="password"
-      :error-messages="emailErrors"
-      label="Password"
-      required
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
-    ></v-text-field>
+  <v-app id="btrac">
+    <v-form>
+      <v-container>
+        <v-card elevation="2">
+          <v-col>
+            <v-col class="mx-auto" cols="12" sm="6">
+              <v-text-field
+                v-model="email"
+                :error-messages="emailErrors"
+                label="E-mail"
+                required
+                @input="$v.email.$touch()"
+                @blur="$v.email.$touch()"
+              ></v-text-field>
+            </v-col>
+            <v-col class="mx-auto" cols="12" sm="6">
+              <v-text-field
+                v-model="password"
+                :error-messages="emailErrors"
+                label="Password"
+                required
+                @input="$v.email.$touch()"
+                @blur="$v.email.$touch()"
+              ></v-text-field>
 
-    <v-btn
-      class="mr-4"
-      @click="submit"
-    >
-      submit
-    </v-btn>
-    <v-btn @click="clear">
-      clear
-    </v-btn>
-  </form>
+              <v-btn class="mr-4" @click="submit"> submit </v-btn>
+              <v-btn @click="clear"> clear </v-btn>
+            </v-col>
+          </v-col>
+        </v-card>
+      </v-container>
+    </v-form>
+  </v-app>
 </template>
 
 <script>
-  import { validationMixin } from 'vuelidate'
-  import { required, maxLength, email } from 'vuelidate/lib/validators'
-  import firebase from "firebase";
+import { validationMixin } from "vuelidate";
+import { required, maxLength, email } from "vuelidate/lib/validators";
+import firebase from "firebase";
 
-  export default {
-    mixins: [validationMixin],
+export default {
+  mixins: [validationMixin],
 
-    validations: {
-      name: { required, maxLength: maxLength(10) },
-      email: { required, email },
-    },
-
-    data: () => ({
-      email: '',
-      password: '',
-    }),
-
-    computed: {
-      emailErrors () {
-        const errors = []
-        if (!this.$v.email.$dirty) return errors
-        !this.$v.email.email && errors.push('Must be valid e-mail')
-        !this.$v.email.required && errors.push('E-mail is required')
-        return errors
-      },
-    },
-
-    methods: {
-      submit () {
-        this.$v.$touch()
-        this.login()
-      },
-      clear () {
-        this.$v.$reset()
-        this.name = ''
-        this.email = ''
-        this.select = null
-        this.checkbox = false
-      },
-      login() {
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(this.email, this.password)
-      .then(() => {
-        alert('Successfully logged in');
-        this.$router.push('/dashboard');
-      })
-      .catch(error => {
-        alert(error.message);
-      });
+  validations: {
+    name: { required, maxLength: maxLength(10) },
+    email: { required, email },
   },
-    },
 
-  }
+  data: () => ({
+    email: "",
+    password: "",
+  }),
+
+  computed: {
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.email && errors.push("Must be valid e-mail");
+      !this.$v.email.required && errors.push("E-mail is required");
+      return errors;
+    },
+  },
+
+  methods: {
+    submit() {
+      this.$v.$touch();
+      this.login();
+    },
+    clear() {
+      this.$v.$reset();
+      this.name = "";
+      this.email = "";
+      this.select = null;
+      this.checkbox = false;
+    },
+    login() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          alert("Successfully logged in");
+          this.$router.push("/dashboard");
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    },
+  },
+};
 </script>

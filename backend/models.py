1355fa39e707ec04e19a48b98802255580a1ce08
sqmch <sqmchh@gmail.com>
2021-04-21 05:57:22 +0100

    Added JWT authentication, swapped out firebase, misc changes

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from settings import *
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 import json
 
 # Initializing our database
@@ -60,9 +61,32 @@ class Issue(db.Model):
         db.session.commit()
 
 
-class UserModel(db.Model):
+class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(50))
-    
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = generate_password_hash(password, method="sha256")
+
+    @classmethod
+    def authenticate(cls, **kwargs):
+        email = kwargs.get("email")
+        password = kwargs.get("password")
+
+        if not email or not password:
+            return None
+
+        user = cls.query.filter_by(email=email).first()
+        if not user or not check_password_hash(user.password, password):
+            return None
+
+        return user
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            email=self.email,
+        )
--- backend/settings.py
+++ backend/settings.py
@@ -10,3 +10,6 @@ CORS(app)
 # configure db
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config[
+    "SECRET_KEY"
+] = "\x80\xfbu\xa5U\xed4k\x98\x94m\x93\x14s:\xb6\x85\xce@/\xdc\xcb\xa3\xd1"
